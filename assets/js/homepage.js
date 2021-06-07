53e0ab11c4bdcb85b6dc1c8db4e04e1fea809b4e
chasem <cpmcquown@gmail.com>
2021-06-07 15:22:34 -0700

    added ability to fecth any users repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,21 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+
+// The getUseRepo functions sole purpose is to retrieve the data that we need, in order to obtain a user's GitHub repositories
+var getUserRepos = function(user) {
+    
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      
+        response.json().then(function(data) {
+        
+            console.log(data);
+      
+        });
+    
+    });
+  
 };
 
-getUserRepos(); 
+getUserRepos("chasemcquown"); 
