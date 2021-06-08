74c7b8eb49106e4942053ed35f8dea5333a546d3
chasem <cpmcquown@gmail.com>
2021-06-08 13:00:57 -0700

    created a way for user to visist github for given repo if more than 30 issues exist

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,85 +1,92 @@
+var repoNameEl = document.querySelector("#repo-name");
 // get reference to issue container
 var issueContainerEl = document.querySelector("#issues-container");
+// get reference to limit-warning container
+var limitWarningEl = document.querySelector("#limit-warning");
 
 // create function to get repository issues from searched user
 var getRepoIssues = function(repo) {
+  // create a variable to hold the query
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-    // create a variable to hold the query
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+  // check the info returned in the response
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+         //  call and pass response data to the displayIssues function
+        displayIssues(data);
 
-    // check the info returned in the response
-    fetch(apiUrl).then(function(response) {
-        
-        // request was successful
-        if (response.ok) {
-            
-            response.json().then(function(data) {
-                
-                //  call and pass response data to the displayIssues function
-                displayIssues(data);
-                
-                console.log(data);
-
-            });
-        // request was unsuccessful    
-        } else {
-
-            alert("There was a problem with your request!");
-
-        };
-    });
-
-    console.log(repo);
+        // check if api has paginated issues. The following code will return the value of link, if that header exists.
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
+      });
+    }
+    // request was unsuccessful
+    else {
+      console.log(response);
+      alert("There was a problem with your request!");
+    }
+  });
 };
 
 // function for displaying the issues to the webpage
 var displayIssues = function(issues) {
+  // check if there are no existing issues within a repository
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
 
-    // check if there are no existing issues within a repository
-    if (issues.length === 0) {
-        
-        issueContainerEl.textContent = "This repo has no open issues";
-        
-        return;
-    }; 
+  // loop over the response data and create an <a> element for each issue identified
+  for (var i = 0; i < issues.length; i++) {
+    // create a link element to take users to the issue on github
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+    
+    // create span to hold issue title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+    
+  
+    // append to container
+    issueEl.appendChild(titleEl);
 
+    // create a type element
+    var typeEl = document.createElement("span");
+
+    // check if issue is an actual issue or a pull request
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull request)";
+    }
+    else {
+      typeEl.textContent = "(Issue)";
+    }
+
+    // append to container
+    issueEl.appendChild(typeEl);
+  
+    // append to the dom
     issueContainerEl.appendChild(issueEl);
-
-    // loop over the response data and create an <a> element for each issue identified
-    for (var i = 0; i< issues.length; i++) {
-        
-        // create a link element to take users to the issue on GitHub
-        var issueEl = document.createElement("a");
-        issueEl.classList = "list-item flex-row justify-space-between align-center";
-        // issue objects have an html_url property that will link to the full issue on GitHub. In other words, the second parameter here is the link for the first parameter which is the href
-        issueEl.setAttribute("href", issues[i].html_url);
-        issueEl.setAttribute("target", "_blank");
-
-        // create a span to hold the issue title
-        var titleEl = document.createElement("span");
-        // make the span display the the issue title that we fetched
-        titleEl.textContent = issues[i].title;
-
-        //append to container
-        issueEl.appendChild(titleEl);
-
-        // create a type element 
-        var typeEl = document.createElement("span");
-
-        // check if issue is an actual issue or a pull request
-        if (issues[i].pull_request) {
-            typeEl.textContent = "(Pull request)";
-        } else {
-            typeEl.textContent = "(Issue)";
-        };
-
-        // append to container
-        issueEl.appendChild(typeEl);
-
-
-
-    };
-
+  }
 };
 
-getRepoIssues("chasemcquown");
+// this functions purpose will be to display a warning if a repo has more than 30 issues
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  // create link element
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
